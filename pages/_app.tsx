0b7c6c337d20c8265df53c072011106082c8c39b
Ludo1815 <ludo48@windowslive.com>
2025-04-22 15:41:49 +0200

    Ajout du tsconfig.json pour TypeScript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { AppProps } from 'next/app';
 import { ClerkProvider } from '@clerk/nextjs';
 import { useRouter } from 'next/router';
@@ -7,7 +6,25 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
 
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={{
+      signIn: {
+        start: {
+          title: "Connexion à votre espace",
+          subtitle: "Entrez votre adresse email pour continuer",
+          actionText: "Continuer",
+        },
+        alternativeMethods: {
+          title: "Ou utilisez une autre méthode",
+        },
+      },
+      signUp: {
+        start: {
+          title: "Créer un compte",
+          subtitle: "Saisissez votre email pour créer un compte",
+          actionText: "S'inscrire",
+        },
+      }
+    }}>
       <Component {...pageProps} key={pathname} />
     </ClerkProvider>
   );
