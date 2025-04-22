ca39bafae44c9aea9c98164558b8e6214e145df0
Ludo1815 <ludo48@windowslive.com>
2025-04-22 15:07:15 +0200

    Fix: ajout de ClerkProvider global

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+// pages/_app.tsx
+import type { AppProps } from 'next/app';
+import { ClerkProvider } from '@clerk/nextjs';
+import { useRouter } from 'next/router';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} key={pathname} />
+    </ClerkProvider>
+  );
+}
